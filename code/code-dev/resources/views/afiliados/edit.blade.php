e37ffdaa27962f44bfd2070d5d5e329fa6e66b9f
EdwinPG2 <112739136+EdwinPG2@users.noreply.github.com>
2022-12-02 11:22:58 -0600

    afiliado actualizar
    se actualizaron las verificaciones de actualizar afiliado

--- code/code-dev/resources/views/afiliados/edit.blade.php
+++ code/code-dev/resources/views/afiliados/edit.blade.php
@@ -17,58 +17,76 @@
                     <div class="row">
                         <div class="col-lg-2 col-md-2">
                             <div class="form-group">
-                                <label for="no_afiliado">Cui</label>
+                                <label for="no_afiliado">Numero de afiliacion (*)</label>
                                 <input type="text" name="no_afiliado" id="no_afiliado" class="form-control" placeholder="" 
-                                value="{{isset($afiliados->no_afiliado)?$afiliados->no_afiliado:''}}">   
+                                value="{{isset($afiliados->no_afiliado)?$afiliados->no_afiliado:''}}" placeholder="Ejemplo: 54321" required maxlength="15" pattern="[0-9]*" title="Ingrese solamente numeros">   
                             </div>
                         </div>
                         <div class="col-lg-2 col-md-2">
                             <div class="form-group">
-                                <label for="cui">Cui</label>
+                                <label for="cui">Cui (*)</label>
                                 <input type="text" name="cui" id="cui" class="form-control" placeholder="" 
-                                value="{{isset($afiliados->cui)?$afiliados->cui:''}}">   
+                                value="{{isset($afiliados->cui)?$afiliados->cui:''}}" required minlength="13" maxlength="13" pattern="[0-9]*" title="Ingrese solamente numeros">   
                             </div>
                         </div>
                         <div class="col-lg-5 col-md-5">
                             <div class="form-group">
-                                <label for="nombre">Nombres</label>
+                                <label for="nombre">Nombres (*)</label>
                                 <input type="text" name="nombre" id="nombre" class="form-control" placeholder=""
-                                value="{{isset($afiliados->nombre)?$afiliados->nombre:''}}">   
+                                value="{{isset($afiliados->nombre)?$afiliados->nombre:''}}" placeholder="Ejemplo: Luis" required max="100">   
                             </div>
                         </div>
                         <div class="col-lg-5 col-md-5">
                             <div class="form-group">
-                                <label for="apellidos">Apellidos</label>
+                                <label for="apellidos">Apellidos (*)</label>
                                 <input type="text" name="apellidos" id="apellidos" class="form-control" placeholder=""
-                                value="{{isset($afiliados->apellidos)?$afiliados->apellidos:''}}">   
+                                value="{{isset($afiliados->apellidos)?$afiliados->apellidos:''}}" placeholder="Ejemplo: Rodriguez" required max="100">   
                             </div>
                         </div>
                         <div class="col-lg-4 col-md-4">
                             <div class="form-group">
-                                <label for="telefono">Telefono</label>
+                                <label for="telefono">Telefono (*)</label>
                                 <input type="text" name="telefono" id="telefono"class="form-control" placeholder=""
-                                value="{{isset($afiliados->telefono)?$afiliados->telefono:''}}">   
+                                value="{{isset($afiliados->telefono)?$afiliados->telefono:''}}" placeholder="8765" required minlength="8" maxlength="15" pattern="[0-9]*" title="Ingrese solamente numeros">   
                             </div>
                         </div>
                         <div class="col-lg-4 col-md-4">
                             <div class="form-group">
                                 <label for="direccion">Direccion</label>
                                 <input type="text" name="direccion" id="direccion" class="form-control" placeholder=""
-                                value="{{isset($afiliados->direccion)?$afiliados->direccion:''}}">   
+                                value="{{isset($afiliados->direccion)?$afiliados->direccion:''}}" maxlength="50" placeholder="Quetzaltenango">   
                             </div>
                         </div>
                         <div class="col-lg-4 col-md-4">
                             <div class="form-group">
                                 <label for="genero">Genero</label>
-                                <input type="text" name="genero" id="genero" class="form-control" placeholder="M - F"
-                                value="{{isset($afiliados->genero)?$afiliados->genero:''}}">   
+                                <select class="form-control" name="genero" id="genero">
+                                    @if ($afiliados->genero=="Masculino")
+                                    {
+                                    <option value="" disabled >-- --</option>
+                                    <option value="Masculino" selected>Masculino</option>
+                                    <option value="Femenino">Femenino</option>
+                                    }
+                                    @elseif($afiliados->genero=="Femenino")
+                                    <option value="" disabled selected>-- --</option>
+                                    <option value="Masculino">Masculino</option>
+                                    <option value="Femenino">Femenino</option>
+                                    @else
+                                    {
+                                        <option value="" disabled selected>-- --</option>
+                                        <option value="Masculino">Masculino</option>
+                                        <option value="Femenino">Femenino</option>
+                                    }
+                                    @endif
+                                    
+                                </select>   
                             </div>
                         </div>
                         <div class="col-lg-4 col-md-4">
                             <div class="form-group">
-                                <label for="fecha_nacimiento">Fecha de nacimiento</label>
+                                <label for="fecha_nacimiento">Fecha de nacimiento (*)</label>
                                 <input type="date" name="fecha_nacimiento" id="fecha_nacimiento" class="form-control" placeholder=""
-                                value="{{isset($afiliados->fecha_nacimiento)?$afiliados->fecha_nacimiento->toDateString():''}}">   
+                                value="{{isset($afiliados->fecha_nacimiento)?$afiliados->fecha_nacimiento->toDateString():''}}" required>   
                             </div>
                         </div>
                         
@@ -82,4 +100,8 @@
         </div>
     </div>
 </div>
+
+<style>
+    input:invalid { border-color: red; } input , input:valid { border-color: #ccc; }
+</style>
 @endsection
