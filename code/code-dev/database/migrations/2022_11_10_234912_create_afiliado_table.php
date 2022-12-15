b90b951585b4a9616f4f7144013e2ffe0ab7e210
thony_GT <thony@mesoamericana.edu.gt>
2022-12-15 00:09:02 -0600

    campo reglon
    se agrego el campo reglon en la tabla de afiliado

--- code/code-dev/database/migrations/2022_11_10_234912_create_afiliado_table.php
+++ code/code-dev/database/migrations/2022_11_10_234912_create_afiliado_table.php
@@ -27,6 +27,7 @@ class CreateAfiliadoTable extends Migration
             $table->string('dependencia',50)->nullable();
             $table->integer('id_cargo')->nullable();
             $table->enum('colaborador', ['y', 'n'])->nullable();
+            $table->string('reglon', 7)->nullable();
             
             $table->foreign('id_cargo', 'afiliado_ibfk_2')->references('id_cargo')->on('cargo')->onDelete('cascade');
             $table->foreign('id_tipo_afiliado', 'fk_afiliado_tipo_afiliado1')->references('Id_tipo_afiliado')->on('tipo_afiliado')->onDelete('cascade');
