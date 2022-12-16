c1d13c75bd863d528c225dbed0b92f2ddfe0019c
thony_GT <thony@mesoamericana.edu.gt>
2022-12-15 19:52:24 -0600

    maxLength de crear afiliado

--- code/code-dev/resources/views/afiliados/create.blade.php
+++ code/code-dev/resources/views/afiliados/create.blade.php
@@ -17,13 +17,13 @@
                     <div class="col-lg-3 col-md-3">
                         <div class="form-group">
                             <label for="no_afiliado">Numero de afiliación (*)</label>
-                            <input type="text" name="no_afiliado" id="no_afiliado" class="form-control" placeholder="Ejemplo: 54321" required maxlength="15" pattern="[0-9]*" title="Ingrese solamente numeros">   
+                            <input type="text" name="no_afiliado" id="no_afiliado" class="form-control" placeholder="Ejemplo: 54321" required maxlength="11" pattern="[0-9]*" title="Ingrese solamente numeros">   
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-3">
                         <div class="form-group">
                             <label for="cui">Cui</label>
-                            <input type="text" name="cui" id="cui" class="form-control" placeholder="Ejemplo: 12345" minlength="13" maxlength="13" pattern="[0-9]*" title="Ingrese solamente numeros">   
+                            <input type="text" name="cui" id="cui" class="form-control" placeholder="Ejemplo: 12345" maxlength="13" pattern="[0-9]*" title="Ingrese solamente numeros">   
                         </div>
                     </div>
                     <div class="col-lg-6 col-md-6">
