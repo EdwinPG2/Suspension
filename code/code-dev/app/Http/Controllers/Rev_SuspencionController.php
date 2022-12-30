0a93c35a79677dc292a20c22a97e762e8b322175
thony_GT <thony@mesoamericana.edu.gt>
2022-12-30 12:29:05 -0600

    auth pago

--- code/code-dev/app/Http/Controllers/Rev_SuspencionController.php
+++ code/code-dev/app/Http/Controllers/Rev_SuspencionController.php
@@ -9,6 +9,7 @@ use App\Models\RevSusp;
 use App\Models\Suspension;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class Rev_SuspencionController extends Controller
