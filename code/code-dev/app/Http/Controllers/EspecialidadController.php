b38cd520741f355f318f56745c8f495cc238a9a5
thony_GT <thony@mesoamericana.edu.gt>
2022-12-29 23:52:39 -0600

    bitacora-especialidad

--- code/code-dev/app/Http/Controllers/EspecialidadController.php
+++ code/code-dev/app/Http/Controllers/EspecialidadController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Especialidad;
+use App\Models\Bitacora;
 use Illuminate\Support\Facades\Validator;
 
 class EspecialidadController extends Controller
@@ -47,6 +48,13 @@ class EspecialidadController extends Controller
 
         $especialidad->save();
 
+        $bitacora = new Bitacora();
+        $bitacora->id_usuario = Auth::user()->id;
+        $bitacora->fecha_hora = Carbon::now()->format('Y/m/d');
+        $bitacora->accion = 'Creación de especialidad';
+        $bitacora->descripcion = 'Creación de la especialidad '.$especialidad->nombre_especialidad;
+        $bitacora->save();
+
         alert()->success('Especialidad guardado correctamente');
 
         return redirect()->route('especialidades.index');
@@ -75,6 +83,13 @@ class EspecialidadController extends Controller
         $especialidad->descripcion = $request->get('descripcion');
         $especialidad->save();
 
+        $bitacora = new Bitacora();
+        $bitacora->id_usuario = Auth::user()->id;
+        $bitacora->fecha_hora = Carbon::now()->format('Y/m/d');
+        $bitacora->accion = 'Actualización de especialidad';
+        $bitacora->descripcion = 'Actualización de la especialidad '.$id;
+        $bitacora->save();
+
         alert()->success('Especialidad actualizado correctamente');
 
         return redirect()->route('especialidades.index');
@@ -85,6 +100,13 @@ class EspecialidadController extends Controller
     {
         $especialidad = Especialidad::find($id);
         $especialidad->delete();
+
+        $bitacora = new Bitacora();
+        $bitacora->id_usuario = Auth::user()->id;
+        $bitacora->fecha_hora = Carbon::now()->format('Y/m/d');
+        $bitacora->accion = 'Eliminación de especialidad';
+        $bitacora->descripcion = 'Eliminación de la especialidad '.$id;
+        $bitacora->save();
 
         alert()->success('Especialidad eliminado correctamente');
         
