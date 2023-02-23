019a9f6385b11e59281899077c216bd03199dd8d
thony_GT <thony@mesoamericana.edu.gt>
2023-02-23 12:23:40 -0600

    Correcciones ortograficas
    ortografia en los reportes

--- code/code-dev/resources/views/reportes/index.blade.php
+++ code/code-dev/resources/views/reportes/index.blade.php
@@ -9,11 +9,11 @@
             <label for="reporte">Seleccione reporte</label>
             <select class="form-control" name="reporte" id="reporte" onchange="javascript:showContent()">
                 <option value="0" selected>-- Seleccione reporte --</option>
-                <option value="1">Suspenciones por area, especialidad, clinica/servicio</option>
-                <option value="2">Suspenciones de colaboradores</option>
-                <option value="3">Suspenciones rechazadas de registradores</option>
-                <option value="4">Suspenciones rechazadas de revisores</option>
-                <option value="5">Suspenciones rechazadas por area, especialidad, clinica/servicio</option>
+                <option value="1">Suspensiones por área, especialidad, clínica/servicio</option>
+                <option value="2">Suspensiones de colaboradores</option>
+                <option value="3">Suspensiones rechazadas de registradores</option>
+                <option value="4">Suspensiones rechazadas de revisores</option>
+                <option value="5">Suspensiones rechazadas por área, especialidad, clínica/servicio</option>
                 <option value="6">Requerimientos generados, en espera y respondidos</option>
                 <option value="7">Requerimientos de pago de suspensiones</option>
 
